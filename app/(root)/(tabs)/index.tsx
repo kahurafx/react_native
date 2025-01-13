edc32142dcddd59f4ee35bbce4ff662e3e521dee
kahurafx <deniskahura44@gmail.com>
2025-01-13 21:46:26 +0300

    Small additional changes

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,17 +1,28 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import {Image, Text, View} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+import images from "@/constants/images";
 
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text className="font-rubik-bold text-2xl py-10">Welcome to Restate</Text>
-        </View>
+        <SafeAreaView className={`bg-white h-full`}>
+            <View className={`px-5`}>
+                <View className={`flex flex-crow items-center justify-between mt-5`}>
+                    <View className={`flex flex-row items-center`}>
+                        <Image source={images.avatar} className={`size-12 rounded-full`}/>
+                        <View className={`flex flex-col items-start ml-2 justify-center`}>
+                            <Text className={`text-xs font-rubik text-black-100`}>
+                                Good Morning
+                            </Text>
+
+                            <Text className={`text-base font-rubik-medium`}>
+                                Cross
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
